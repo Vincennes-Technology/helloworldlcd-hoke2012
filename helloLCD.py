27677531fe2a1e97c67431e76fc03c0bec6b6d43
hoke2012 <caleb.hoke@hotmail.com>
2018-03-19 16:45:14 +0000

    displayText issues

--- helloLCD.py
+++ helloLCD.py
@@ -5,29 +5,31 @@
 import Adafruit_CharLCD as LCD
 import time
 import subprocess
-
+  # lint:ok
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-displayText = "CalebLikesPi\n"
+Name = subprocess.check_output("hostname").strip()
+displayText = Name
+  # lint:ok
 
 IP = subprocess.check_output(["hostname", "-I"])
 
 lcd.clear()
-lcd.message(displayText)
+lcd.message("displayText\n")
 
 while(True):
     if lcd.is_pressed(LCD.SELECT):
         lcd.clear()
-        lcd.message(displayText)
+        lcd.message("displayText\n")
         lcd.set_backlight(1)
-        lcd.message(    "Hello World\n")
-        time.sleep(2.0)
+        lcd.message("Hello World\n")
+        time.sleep(0.5)
     else:
         lcd.set_backlight(1)
         lcd.message(displayText)
         lcd.message(IP)
-        time.sleep(2.0)
+        time.sleep(0.5)
         lcd.clear()
 
 
