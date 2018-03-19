03aeccf79e99cb416a0843093e400a93f5b2db0f
hoke2012 <caleb.hoke@hotmail.com>
2018-03-19 16:15:54 +0000

    added if & else added time.sleep

--- helloLCD.py
+++ helloLCD.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python
-#show "Hello World on the LCD screen"
+#show IP address on the LCD Plate at startup
 #CHoke
 
 import Adafruit_CharLCD as LCD
+import time
+import subprocess
+
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-displayText = "Caleb Loves Pi"
+displayText = "CalebLikesPi\n"
+
+IP = subprocess.check_output(["hostname", "-I"])
 
 lcd.clear()
 lcd.message(displayText)
+
+while(True):
+    if lcd.is_pressed(LCD.SELECT):
+        lcd.clear()
+        lcd.message(displayText)
+        lcd.set_backlight(1)
+        lcd.message(    "Hello World\n")
+        time.sleep(2.0)
+    else:
+        lcd.set_backlight(1)
+        lcd.message(displayText)
+        lcd.message(IP)
+        time.sleep(2.0)
+        lcd.clear()
+
 
